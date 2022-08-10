54cd93953d4bb47e7f67559b1395f326d5a6e84e
Joanna Leng <j.leng@leeds.ac.uk>
2022-08-10 12:01:36 +0100

    Converted the file tests/physics/sphere_diffussion/diffussion_mass.py from python 2 to python 3 so that it executes in the ffeatools python 3 version virtual environment, so it imports the new ffeatools correctly and so it has a good pylint score of over 9.

--- tests/physics/sphere_diffusion/diffusion_mass.py
+++ tests/physics/sphere_diffusion/diffusion_mass.py
@@ -1,79 +1,89 @@
-# 
-#  This file is part of the FFEA simulation package
-#  
-#  Copyright (c) by the Theory and Development FFEA teams,
-#  as they appear in the README.md file. 
-# 
-#  FFEA is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-# 
-#  FFEA is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-# 
-#  You should have received a copy of the GNU General Public License
-#  along with FFEA.  If not, see <http://www.gnu.org/licenses/>.
-# 
-#  To help us fund FFEA development, we humbly ask that you cite 
-#  the research papers on the package.
-#
+#!/usr/bin/env python
+"""
+This script tests the diffusion of a sphere with mass.
+As understood when converting to Python 3 in August 2022.
 
-import numpy as np
+  This file is part of the FFEA simulation package
+
+  Copyright (c) by the Theory and Development FFEA teams,
+  as they appear in the README.md file.
+
+  FFEA is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  FFEA is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with FFEA.  If not, see <http://www.gnu.org/licenses/>.
+
+  To help us fund FFEA development, we humbly ask that you cite
+  the research papers on the package.
+"""
+
 import sys
+import numpy as np
 
-ffeatoolsFound = False
-try:
-    import ffeatools # python package
-    ffeatoolsFound = True
-    FFEA_script = ffeatools.modules.FFEA_script
-    FFEA_material = ffeatools.modules.FFEA_script
-except:
-    try:
-        import FFEA_script, FFEA_material
-    except ImportError:
-        print("Failure to import relevent FFEA modules")
-        sys.exit(1) # failure to import
+#FFEATOOLS_FOUND = False
+#try:
+#    import ffeatools # python package
+#    FFEATOOLS_FOUND = True
+#    FFEA_script = ffeatools.ffea_script_jl
+#    FFEA_material = ffeatools.ffea_material_jl
+#except ImportError:
+#    print("diffussion_mass.py: Failure to import relevent FFEAtools modules")
+#    sys.exit(1) # failure to import
+    
+    
+from ffeatools import ffea_script_jl as ffea_script_jl
+from ffeatools import ffea_material_jl as ffea_material_jl
 
 # Load trajectory
-start = 5000
-#start = 0
-end = 10000
-try:
-	script = FFEA_script.FFEA_script(sys.argv[1])
-except:
-	sys.exit("Script please!")
-traj = script.load_trajectory(start=start, num_frames = end-start)
-end = traj.num_frames + start
-mat = script.load_material(0)
-top = script.load_topology(0)
+START = 5000
+# START = 0
+END = 10000
+script = ffea_script_jl.ffea_script_jl(sys.argv[1])
+#try:
+#    script = FFEA_script.ffea_script_jl(sys.argv[1])
+#except:
+#    sys.exit("diffussion_mass.py: Failure to process ffea script!")
+
+trajectory = script.load_trajectory(start=START, num_frames = END-START)
+END = trajectory.num_frames + START
+material = script.load_material(0)
+topology = script.load_topology(0)
 
 # Analyse trajctory in sets of 1ps and test against theoretical diffusion
-r2 = [None for i in range(start, end-1)]
+r2 = [None for i in range(START, END-1)]
 
-for f in traj.blob[0][0].frame:
-	f.calc_centroid()
+for f in trajectory.blob[0][0].frame:
+    f.calc_centroid()
 
-for i in range((end - start) - 1):
-	r2[i] = traj.blob[0][0].frame[i + 1].get_centroid() - traj.blob[0][0].frame[i].get_centroid()
-	r2[i] = np.dot(r2[i], r2[i])
+for index in range((END - START) - 1):
+    r2[index] = (trajectory.blob[0][0].frame[index + 1].get_centroid() -
+             trajectory.blob[0][0].frame[index].get_centroid())
+    r2[index] = np.dot(r2[index], r2[index])
 
 r2mean = np.mean(r2, axis=0)
 r2stdev = np.std(r2, axis=0)
 r2err = r2stdev / np.sqrt(len(r2))
 
-mass = traj.blob[0][0].frame[0].calc_mass(top, mat)
+mass = trajectory.blob[0][0].frame[0].calc_mass(topology, material)
 r2theory = 3 * (script.params.kT / mass) * ((script.params.dt * script.params.check)**2)
 #print "Calculated Diffusion: <r^2> = %6.2e +/- %6.2e" % (r2mean, r2err)
 #print "Theoretical Diffusion: <r^2> = %6.2e" % (r2theory)
 #print "Percent Error: d<r^2>/<r^2> = %6.2f%%" % (100 * np.fabs(r2mean - r2theory) / r2theory)
-err = 0
-tolerance = 0.1
-if np.fabs(r2mean - r2theory) / r2theory <= tolerance:
-	print "Inertial Diffusion", ": correct ", np.fabs(r2mean - r2theory) / r2theory , " < ", tolerance
+ERROR = 0
+TOLERANCE = 0.1
+if np.fabs(r2mean - r2theory) / r2theory <= TOLERANCE:
+    print("Inertial Diffusion: correct \n"
+          "\t {np.fabs(r2mean - r2theory) / r2theory}  < {TOLERANCE}")
 else:
-	print "Inertial Diffusion", ": failed ", np.fabs(r2mean - r2theory) / r2theory , " > ", tolerance
-	err = 1
-sys.exit(err)
+    print("Inertial Diffusion: failed \n"
+          "\t {np.fabs(r2mean - r2theory) / r2theory}  > {TOLERANCE}")
+    ERROR = 1
+sys.exit(ERROR)
