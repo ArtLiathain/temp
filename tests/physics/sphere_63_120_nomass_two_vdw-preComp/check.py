4ec830b4163dfe5ac908d9963582f87db5f759e0
Joanna Leng <j.leng@leeds.ac.uk>
2022-08-10 11:47:03 +0100

    Converted the file tests/physics/sphere_63_120_nomass_two-preComp/check.py from python 2 to python 3 so that it executes in the ffeatools python 3 version virtual environment and so it has a good pylint score of over 9.

--- tests/physics/sphere_63_120_nomass_two_vdw-preComp/check.py
+++ tests/physics/sphere_63_120_nomass_two_vdw-preComp/check.py
@@ -1,84 +1,92 @@
-# 
-#  This file is part of the FFEA simulation package
-#  
-#  Copyright (c) by the Theory and Development FFEA teams,
-#  as they appear in the README.md file. 
-# 
-#  FFEA is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-# 
-#  FFEA is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-# 
-#  You should have received a copy of the GNU General Public License
-#  along with FFEA.  If not, see <http://www.gnu.org/licenses/>.
-# 
-#  To help us fund FFEA development, we humbly ask that you cite 
-#  the research papers on the package.
-#
+#!/usr/bin/env python
+"""
+  This file is part of the FFEA simulation package
+
+  Copyright (c) by the Theory and Development FFEA teams,
+  as they appear in the README.md file.
+
+  FFEA is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  FFEA is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with FFEA.  If not, see <http://www.gnu.org/licenses/>.
+
+  To help us fund FFEA development, we humbly ask that you cite
+  the research papers on the package.
+"""
 
 import sys
 
 
-def readAndStore(iFile):
-  H = []
-  dH = {}
-  with open(iFile, 'r') as sta:
-    while (sta.readline() != "Measurements:\n"):
-      continue
+def read_and_store(file_in):
+    """
+    This function reads the file in and stores the data. As understood when converting to
+    Python 3 in August 2022.
+    """
+    data = []
+    changed_data = {}
+    with open(file_in, 'r') as file:
+        while file.readline() != "Measurements:\n":
+            continue
 
-    blob = ""
-    readblob = False
-    for i in sta.readline().split():
-      if i == "|":
-        readblob = True
-        continue
-      if readblob:
-        blob = i
+        blob = ""
         readblob = False
-        print(i)
-        continue
-      H.append([blob+i])
-    for line in sta:
-      cnt = 0
-      if line.count("RESTART"): continue
-      for i in line.split():
-        H[cnt].append(float(i))
-        cnt += 1
+        for item in file.readline().split():
+            if item == "|":
+                readblob = True
+                continue
+            if readblob:
+                blob = item
+                readblob = False
+                print(item)
+                continue
+            data.append([blob+item])
+        for line in file:
+            cnt = 0
+            if line.count("RESTART"):
+                continue
+            for item in line.split():
+                data[cnt].append(float(item))
+                cnt += 1
 
-  for i in range(len(H)):
-    dH[H[i][0]] = i
-    H[i].pop(0)
+    for item in range(len(data)):
+        changed_data[data[item][0]] = item
+        data[item].pop(0)
 
-  return H, dH
+    return data, changed_data
 
 
 ## compare the PreCompEnergy of the first step to a value that has been checked:
-err = 0
-knownValue = 2.79280200000000000e-18
-#knownValue = 1.0075630000000001e-18
-#knownValue = 6.87781100000000001e-17
+ERROR = 0
+KNOWN_VALUE = 2.79280200000000000e-18
+#KNOWN_VALUE = 1.0075630000000001e-18
+#KNOWN_VALUE = 6.87781100000000001e-17
 
-## ## ## STRAIGHT ## ## ## 
-iFile = ["sphere_63_120_two_measurement.out"]
-H, dH = readAndStore(iFile[0])
+## ## ## STRAIGHT ## ## ##
+filein = ["sphere_63_120_two_measurement.out"]
+H, dH = read_and_store(filein[0])
 
-if ( abs (H[dH["PreCompEnergy"]][-1] - knownValue) > 1e-32 ): 
-  print( "PreComputed energy should be %e, but was found to be %e" % (knownValue, (H[dH["PreCompEnergy"]][-1])))
-  err = 1
-
-
-## ## ## PBC ## ## ## 
-iFile = ["sphere_63_120_two-PBC_measurement.out"]
-H, dH = readAndStore(iFile[0])
-if ( abs (H[dH["PreCompEnergy"]][-1] - knownValue) > 1e-32 ): 
-  print( "PBC PreComputed energy should be %e, but was found to be %e" % (knownValue, (H[dH["PreCompEnergy"]][-1])))
-  err = 1
+if abs (H[dH["PreCompEnergy"]][-1] - KNOWN_VALUE) > 1e-32 :
+    print("PreComputed energy should be {KNOWN_VALUE}, "
+          "but was found to be {(H[dH['PreCompEnergy']][-1]))}")
+    ERROR = 1
 
 
+## ## ## PBC ## ## ##
+filein = ["sphere_63_120_two-PBC_measurement.out"]
+H, dH = read_and_store(filein[0])
+if abs (H[dH["PreCompEnergy"]][-1] - KNOWN_VALUE) > 1e-32 :
+    print("PBC PreComputed energy should be {KNOWN_VALUE}, "
+          "but was found to be {(H[dH['PreCompEnergy']][-1])}")
+    ERROR = 1
 
-sys.exit(err)
+
+
+sys.exit(ERROR)
